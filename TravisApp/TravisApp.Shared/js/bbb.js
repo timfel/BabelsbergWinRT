f246af12016c97b9c818c369c282c1411d3bbc35
Tim Felgentreff <timfelgentreff@gmail.com>
2014-12-05 19:52:23 +0800

    add commented constraints

--- TravisApp/TravisApp.Shared/js/bbb.js
+++ TravisApp/TravisApp.Shared/js/bbb.js
@@ -6,4 +6,16 @@
             ui.model === ea.model
         }
     });
+
+    // Below does not currently work, because the dependencies aren't
+    // setup right. It should really recalc when ui.model changes, but it
+    // doesn't
+    //for (var i = 0; i < ui.progressbars.length; i++) {
+    //    always: {
+    //        solver: d
+    //        ui.progressbars[i].innerText.formula([ui.model, ui.model.progress[i]], function (m, p) {
+    //            ui.progressbars[i].textContent = ui.model.progress[i] + "%";
+    //        });
+    //    }
+    //};
 };
